70d68725cf505fc71db0d3bae032cc045a2f2420
Otto Urpelainen <oturpe@iki.fi>
2016-11-17 21:41:52 +0100

    Upgraded version

--- fuel-cell-monitor.ino
+++ fuel-cell-monitor.ino
@@ -7,19 +7,28 @@
 // Pin for button that outputs saved readings
 #define CONTROL_BUTTON_PIN 2
 // Pin for recording indicator
-#define RECORDING_INDICATOR_PIN 13
+#define RECORDING_INDICATOR_PIN 5
 
+// How the control button must be pressed to register a button press
+#define BUTTON_OUTPUT_DELAY 200
 // How the control button must be pressed to start or stop writing values.
 // Given in units of millisecond
-#define WRITE_CONTROL_DELAY 2000
+#define BUTTON_WRITE_DELAY 2000
 
-// How often values are saved. Given in units of millisecond.
-#define WRITE_INTERVAL 1000 
+// How many samples to average for each reading
+#define SAMPLE_COUNT 20
+// How long to wait between samples
+#define SAMPLE_INTERVAL 100
+
+// How often measurements are taken. Given in units of millisecond.
+#define MEASUREMENT_INTERVAL (2*60*1000)
 
 // Size of eeprom memory to read
 #define EEPROM_SIZE 1024
 // Marker to write for write start
 #define WRITE_START_MARK 0xff
+// Marker to write for write end
+#define WRITE_END_MARK 0xfe
 
 bool isWriting = false;
 
@@ -34,7 +43,12 @@ int buttonDownTime = 0;
 // The moment values were last written
 int writeTime = 0;
 
+// How many blocks have been written
+int blocksWritten = 0;
+
 void setup() {
+    analogReference(INTERNAL);
+
     pinMode(V_SENSE_PIN, INPUT);
     pinMode(I_SENSE_PIN, INPUT);
     pinMode(CONTROL_BUTTON_PIN, INPUT_PULLUP);
@@ -51,13 +65,16 @@ void loop() {
     }
     else if (!isButtonDown && wasButtonDown) {
         // Button was released
+        int buttonDownLength = currentTime - buttonDownTime;
 
-        if (currentTime - buttonDownTime > WRITE_CONTROL_DELAY) {
+        if (buttonDownLength > BUTTON_WRITE_DELAY) {
             isWriting = !isWriting;
             if (isWriting) {
                  WriteStartMark();
+            } else {
+                WriteEndMark();
             }
-        } else {
+        } else if (buttonDownLength > BUTTON_OUTPUT_DELAY) {
             outputReadings();
         }
     }
@@ -72,23 +89,38 @@ void loop() {
 }
 
 void WriteStartMark() {
-    if (writeAddress == EEPROM_SIZE) {
+    if (writeAddress == EEPROM_SIZE - 1) {
         return;
     }
 
 	EEPROM.write(writeAddress++, WRITE_START_MARK);
 }
 
+void WriteEndMark() {
+    EEPROM.write(writeAddress++, WRITE_END_MARK);
+    EEPROM.write(EEPROM_SIZE - 1, ++blocksWritten);
+}
+
 void WriteReadings() {
-    if (writeAddress == EEPROM_SIZE) {
+    // Last bit is amount of blocks
+    if (writeAddress == EEPROM_SIZE -1) {
         return;
     }
 
-    int voltage = analogRead(V_SENSE_PIN);
-	voltage = constrain(voltage, 0, 0xfe);
+    int voltage = 0;
+    int current = 0;
+    for (int i = 0; i < SAMPLE_COUNT; i++) {
+        int voltageSample = analogRead(V_SENSE_PIN);
+	    voltage += constrain(voltageSample, 0, 0xfd);
 
-	int current = analogRead(I_SENSE_PIN);
-    current = constrain(current, 0, 0xfe);
+	    int currentSample = analogRead(I_SENSE_PIN);
+        current += constrain(currentSample, 0, 0xfd);
+
+        delay(SAMPLE_INTERVAL);
+    }
+
+    voltage /= SAMPLE_COUNT;
+    current /= SAMPLE_COUNT;
 
     // TODO: Should we fit both readings into 1 byte?
     // So 0...126 for both values?
@@ -102,19 +134,76 @@ bool IsTimeToWrite(int currentTime) {
 	    return true;
 	}
 
-	return currentTime - writeTime > WRITE_INTERVAL;
+	return currentTime - writeTime > MEASUREMENT_INTERVAL;
 }
 
 void outputReadings() {
     Serial.begin(9600);
+    Serial.println("Printing previously recorded values.");
+    Serial.println("Values given in pairs (V, I)");
 
-	for (int address = 0; address < writeAddress; address++) {
-	    byte value = EEPROM.read(address);
-	    if (value == WRITE_START_MARK) {
-	        Serial.println("=== Write started ===");
-	    } else {
-	        Serial.print(value, DEC);
-	        Serial.println();
-	    }
-	}
+    int blocks = EEPROM.read(EEPROM_SIZE - 1);
+    if (!blocks) {
+        Serial.println("No previous recording found.");
+        Serial.end();
+        return;
+    }
+
+    int blockIndex = 0;
+    int valueIndex = 0;
+    int totalValues = 0;
+
+    // Prints voltage and current on the same row. Which one are we
+    // prining now?
+    bool isVoltage = true;
+
+    for (int address = 0; address < EEPROM_SIZE - 1; address++) {
+        byte value = EEPROM.read(address);
+        if (value == WRITE_START_MARK) {
+            Serial.print("Printing values from recording block ");
+            Serial.println(blockIndex++, DEC);
+
+            totalValues += valueIndex;
+            valueIndex = 0;
+        }
+        else if (value == WRITE_END_MARK) {
+            Serial.print("Block ");
+            Serial.print(blockIndex, DEC);
+            Serial.print(" contained ");
+            Serial.print(valueIndex, DEC);
+            Serial.println(" values");
+            
+            if (blockIndex == blocks) {
+                break;
+            }
+        }
+        else {
+            if (isVoltage) {
+                Serial.print(value, DEC);
+                Serial.print("; ");
+                valueIndex++;
+
+                isVoltage = !isVoltage;
+            }
+            else {
+                Serial.print(value, DEC);
+                Serial.println();
+                valueIndex++;
+
+                isVoltage = !isVoltage;
+            }
+        }
+    }
+
+    totalValues += valueIndex;
+
+    Serial.print("Done printing ");
+    Serial.print(totalValues, DEC);
+    Serial.print(" values from ");
+    Serial.print(blockIndex, DEC);
+    Serial.println(" blocks");
+    Serial.end();
+}
+
+void outputBlockEndMark(int blockIndex, int valueIndex) {
 }
