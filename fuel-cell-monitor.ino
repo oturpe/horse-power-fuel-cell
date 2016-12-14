8220eb3519eb4fcac6a820d8b04dff001d42e27d
Otto Urpelainen <oturpe@iki.fi>
2016-12-14 01:35:30 +0100

    Implement writing into the whole display screen
    However, no sensible information is written yet

--- fuel-cell-monitor.ino
+++ fuel-cell-monitor.ino
@@ -15,6 +15,23 @@
 // Analog input pin for temperature sensor
 #define TEMPERATURE_PIN A6
 
+// Liquid crystal display data pins
+#define SCREEN_DB0_PIN 9
+#define SCREEN_DB1_PIN 2
+#define SCREEN_DB2_PIN 8
+#define SCREEN_DB3_PIN 3
+#define SCREEN_DB4_PIN 7
+#define SCREEN_DB5_PIN 4
+#define SCREEN_DB6_PIN 6
+#define SCREEN_DB7_PIN 5
+// Liquid crystal display data/~command pin
+#define SCREEN_DC_PIN 13
+// Liquid crystal display enable pins
+#define SCREEN_ENABLE1_PIN 10
+#define SCREEN_ENABLE2_PIN A0
+// Liquid crystal display reset pin
+#define SCREEN_RESET_PIN 12
+
 // How many quatities are measured
 #define QUANTITY_COUNT 5
 
@@ -49,6 +66,33 @@
 // 0 % .. 100 %.
 #define SOLUTION_PUMP_PWM_DUTY_CYCLE 170
 
+// How often something is written to screen. Given in units of millisecond
+#define SCREEN_UPDATE_PERIOD 100
+
+// Number of columns in the screen controller.
+#define SCREEN_COLUMN_COUNT 61
+// Number of pages in the screen controller.
+#define SCREEN_PAGE_COUNT 4
+// Number of chips in the screen controller.
+#define SCREEN_CHIP_COUNT 2
+
+/// WG12232 command codes
+#define SCREEN_CMD_RMW       0xE0
+#define SCREEN_CMD_END_RMW   0xEE
+#define SCREEN_CMD_SOFT_RST  0xE2
+
+#define SCREEN_CMD_DISP_ON   0xAF
+#define SCREEN_CMD_DISP_OFF  0xAE
+
+#define SCREEN_CMD_MAP_SEG   0xA0
+
+#define SCREEN_CMD_SET_SDL   0xC0
+#define SCREEN_CMD_SEL_PAGE  0xB8
+#define SCREEN_CMD_SEL_COL   0
+
+#define SCREEN_DTM_RW_BIT    2
+#define SCREEN_DTM_CD_BIT    1
+
 bool isWriting = false;
 
 // Eeprom address to write
@@ -70,6 +114,19 @@ unsigned long solutionPumpChangeTime = 0;
 // How many blocks have been written
 int blocksWritten = 0;
 
+// Current start Display Line of the screen
+uint8_t screenStartDisplayLine;
+// Current Column Address of the screen
+uint8_t currentScreenColumn = 0;
+// Current Page Address of the screen
+uint8_t currentScreenPage = 0;
+// Current chip of the screen
+uint8_t currentScreenChip = 0;
+// Enable pin of the current chip
+uint8_t currentScreenChipPin;
+// The last time the screen was updated. Given in units of millisecond
+unsigned long screenLastUpdateTime = 0;
+
 void setup() {
     analogReference(INTERNAL);
 
@@ -82,7 +139,147 @@ void setup() {
     pinMode(SOLUTION_PUMP_PIN, OUTPUT);
     pinMode(RECORDING_INDICATOR_PIN, OUTPUT);
 
+    pinMode(SCREEN_ENABLE1_PIN, OUTPUT);
+    pinMode(SCREEN_ENABLE2_PIN, OUTPUT);
+    pinMode(SCREEN_DC_PIN, OUTPUT);
+    pinMode(SCREEN_RESET_PIN, OUTPUT);
+
+    pinMode(SCREEN_DB0_PIN, OUTPUT);
+    pinMode(SCREEN_DB1_PIN, OUTPUT);
+    pinMode(SCREEN_DB2_PIN, OUTPUT);
+    pinMode(SCREEN_DB3_PIN, OUTPUT);
+    pinMode(SCREEN_DB4_PIN, OUTPUT);
+    pinMode(SCREEN_DB5_PIN, OUTPUT);
+    pinMode(SCREEN_DB6_PIN, OUTPUT);
+    pinMode(SCREEN_DB7_PIN, OUTPUT);
+
+    delay(10);
+    // Initialization code adapted from the display's datasheet
+    digitalWrite(SCREEN_RESET_PIN, LOW);
+    delay(10);
+    digitalWrite(SCREEN_RESET_PIN, HIGH);
+    delay(10);
+    screenEndRmw();
+    setScreenStartDisplayLine(0);
+    displayOn();
+    screenSelectChip(currentScreenChip);
+    screenSelectPage(currentScreenPage);
+
     analogWrite(SOLUTION_PUMP_PIN, 0);
+}
+
+void screenCommand(uint8_t command) {
+    digitalWrite(SCREEN_ENABLE1_PIN, HIGH);
+    digitalWrite(SCREEN_ENABLE2_PIN, HIGH);
+    // Set command mode
+    digitalWrite(SCREEN_DC_PIN, LOW);
+    delayMicroseconds(10);
+    setScreenDataBits(command);
+    digitalWrite(SCREEN_ENABLE1_PIN, LOW);
+    digitalWrite(SCREEN_ENABLE2_PIN, LOW);
+}
+
+void writeScreenData(uint8_t character) {
+    // Set data mode
+    digitalWrite(currentScreenChipPin, HIGH);
+    digitalWrite(SCREEN_DC_PIN, HIGH);
+    // ???
+    delayMicroseconds(10);
+    setScreenDataBits(character);
+    digitalWrite(currentScreenChipPin, LOW);
+}
+
+void setScreenDataBits(uint8_t bits) {
+    digitalWrite(SCREEN_DB0_PIN, bits & 0x01);
+    digitalWrite(SCREEN_DB1_PIN, bits & 0x02);
+    digitalWrite(SCREEN_DB2_PIN, bits & 0x04);
+    digitalWrite(SCREEN_DB3_PIN, bits & 0x08);
+    digitalWrite(SCREEN_DB4_PIN, bits & 0x10);
+    digitalWrite(SCREEN_DB5_PIN, bits & 0x20);
+    digitalWrite(SCREEN_DB6_PIN, bits & 0x40);
+    digitalWrite(SCREEN_DB7_PIN, bits & 0x80);
+}
+
+/**
+ * To switch display on for currently selected chip.
+ */
+inline void displayOn() {
+    screenCommand(SCREEN_CMD_DISP_ON);
+}
+
+/**
+ * To set Start Display Line.
+ * \param line Start Display Line number (0..31).
+ */
+inline void setScreenStartDisplayLine(uint8_t line) {
+    // TODO: How to save the line?
+    screenStartDisplayLine = line;
+    screenCommand(SCREEN_CMD_SET_SDL + line);
+}
+
+/**
+ * To start Read/Modify/Write sequence.
+ */
+inline void screenBeginRmw() {
+    screenCommand(SCREEN_CMD_RMW);
+}
+
+/**
+ * To finish Read/Modify/Write sequence.
+ */
+inline void screenEndRmw() {
+    screenCommand(SCREEN_CMD_END_RMW);
+}
+
+
+/**
+ * To select page. 32 lines are segmented for 4 pages 8 lines each.
+ * \param line Page number (0..3).
+ */
+inline void screenSelectPage(uint8_t page) {
+    screenCommand(SCREEN_CMD_SEL_PAGE + page);
+}
+
+/**
+ * Select the given chip for screen.
+ */
+inline void screenSelectChip(uint8_t chip) {
+    switch (chip) {
+    case 0:
+        currentScreenChipPin = SCREEN_ENABLE1_PIN;
+        break;
+    case 1:
+        currentScreenChipPin = SCREEN_ENABLE2_PIN;
+        break;
+    }
+}
+
+/**
+ * To select column. 122 columns are controled by 2 chips 61 column each.
+ * \param line Column number (0..60).
+ */
+inline void screenSelectColumn(uint8_t col) {
+    screenCommand(SCREEN_CMD_SEL_COL + col);
+}
+
+inline static uint8_t screenColumnFromX(uint8_t x) {
+    return (x > 60) ? x-61 : x;
+}
+
+uint8_t screenPageFromY(uint8_t y)
+{
+  uint8_t line = screenStartDisplayLine + y;
+  if(line > 31)
+    line -= 32;
+  return line / 8;
+}
+
+uint8_t screenBitFromY(uint8_t y)
+{
+  uint8_t line = screenStartDisplayLine + y;
+  if(line > 31)
+    line -= 32;
+  return line % 8;
 }
 
 void loop() {
@@ -118,6 +315,7 @@ void loop() {
     }
 
     NotifySolutionPumpTimer(currentTime);
+    NotifyScreen(currentTime);
 }
 
 void WriteStartMark() {
@@ -268,6 +466,29 @@ void NotifySolutionPumpTimer(unsigned long currentTime) {
     ) {
         analogWrite(SOLUTION_PUMP_PIN, SOLUTION_PUMP_PWM_DUTY_CYCLE);
         ChangeSolutionPumpState(currentTime);
+    }
+}
+
+// TODO: Implement writing something sensible to screen.
+uint8_t currentScreenByte = 0;
+
+void NotifyScreen(unsigned long currentTime) {
+//    if (currentTime <= screenLastUpdateTime + SCREEN_UPDATE_PERIOD) {
+//        return;
+//    }
+//    screenLastUpdateTime = currentTime;
+
+    writeScreenData(currentScreenByte);
+    currentScreenByte = currentScreenByte + 1;
+    currentScreenColumn = (currentScreenColumn + 1) % SCREEN_COLUMN_COUNT;
+    if (currentScreenColumn == 0) {
+        screenSelectColumn(0);
+        currentScreenPage = (currentScreenPage + 1) % SCREEN_PAGE_COUNT;
+        screenSelectPage(currentScreenPage);
+        if (currentScreenPage == 0) {
+            currentScreenChip = (currentScreenChip + 1) % SCREEN_CHIP_COUNT;
+            screenSelectChip(currentScreenChip);
+        }
     }
 }
 
